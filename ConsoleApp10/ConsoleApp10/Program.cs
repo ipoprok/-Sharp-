e4d690c74111b78725416fe3558cc29289d87a63
ipoprok <pandachka1980@gmail.com>
2025-05-20 01:01:15 +0300

    Add files via upload

---
+++ ConsoleApp10/ConsoleApp10/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[,,] mas = {
+            { { 1, 2 }, { 3, 4 } },
+            { { 4, 5 }, { 6, 7 } },
+            { { 7, 8 }, { 9, 10 } },
+            { { 10, 11 }, { 12, 13 } }
+        };
+
+        Console.Write("{");
+
+        for (int i = 0; i < mas.GetLength(0); i++)
+        {
+            Console.Write("{");
+            for (int j = 0; j < mas.GetLength(1); j++)
+            {
+                Console.Write("{");
+                Console.Write($"{mas[i, j, 0]} , {mas[i, j, 1]}");
+                Console.Write("}");
+                if (j != mas.GetLength(1) - 1)
+                    Console.Write(" , ");
+            }
+            Console.Write("}");
+            if (i != mas.GetLength(0) - 1)
+                Console.Write(" , ");
+        }
+
+        Console.WriteLine("}");
+    }
+}
